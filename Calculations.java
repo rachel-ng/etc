c4c66a2281271396da6c3f722935966f1ab0e73d
rng <rng@stuy.edu>
2018-06-25 20:00:00 -0400

    stuff from before

---
+++ Calculations.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Calculations {
+
+    
+    
+    public Calculations () {
+
+    }
+
+    public boolean pair (Hand h, CommunityCards cc) {
+	if 
+    }
+    
+}
+
