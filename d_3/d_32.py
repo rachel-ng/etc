0964994f807e1aaaeeb7c5e20ab0430caed6f3e9
rng <rng@stuy.edu>
2018-12-11 04:06:42 -0500

    sos

---
+++ d_3/d_32.py
@@ -0,0 +1,50 @@
+f = open('input.txt', "r")
+lines = f.readlines()
+f.close()
+
+#print (list(lines))
+
+file_lines = list(lines)
+
+def process(o_line):
+    c_line = o_line[1:].replace("@","").replace(":","").replace(","," ").replace("x"," ").rstrip("\n").split()
+    #print(c_line)
+    return c_line
+
+cleaned_lines = []
+
+for i in file_lines:
+    cleaned_lines.append(process(i))
+
+
+locations = {}
+
+print(['claim number', 'x left', 'x top', 'x wide', 'x tall'])
+
+for i in cleaned_lines:
+    a = int(i[1]) # stay the same
+    b = int(i[2]) # stay the same
+    c = int(i[3])
+    d = int(i[4])
+    while c > 0:
+        x = a + c
+        while d > 0:
+            y = b + d
+            z = str(x) + "," + str(y)
+            print(z)
+            if not(z in locations.keys()):
+                locations[z] = 1
+            else:
+                locations[z] += 1
+            d -= 1
+        d = int(i[4])
+        c -= 1
+
+#print(locations)
+
+sum = 0
+for i in locations.values():
+    if i > 1:
+        sum += 1
+
+print(sum)
