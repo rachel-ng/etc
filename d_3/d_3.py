4e90af6b575895bf72d603cd3cf06093d2f37a5f
rng <rng@stuy.edu>
2018-12-11 03:57:46 -0500

    sos

--- d_3/d_3.py
+++ d_3/d_3.py
@@ -47,7 +47,7 @@ for i in cleaned_lines:
         d = int(i[4])
         c -= 1
 
-print(locations2)
+#print(locations2)
 
 
 baddddddd = {}
@@ -56,6 +56,7 @@ for i in locations2.values():
         baddddddd[i] = ""
 
 #print(bad)
+print(baddddddd)
 
 sum = 0
 for i in locations.values():
