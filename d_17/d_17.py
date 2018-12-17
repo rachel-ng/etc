6aae844f4f05f25384a77afd98b0cbe4beb7cce7
rng <rng@stuy.edu>
2018-12-17 18:57:43 -0500

    sos

--- d_17/d_17.py
+++ d_17/d_17.py
@@ -10,5 +10,21 @@ def process(o_line):
     c_line = o_line.rstrip("\n").replace("x","").replace("y","").replace("=","").split(",")
     return c_line[0], c_line[1][1:]
 
+x = {}
+
+y = {}
+
 for i in file_lines:
-    print(process(i))
+    reeeee = process(i)
+    if not(reeeee[0] in x.keys()):
+        x[reeeee[0]] = 1
+    else:
+        x[reeeee[0]] += 1
+    if not(reeeee[1] in y.keys()):
+        y[reeeee[1]] = 1
+    else:
+        y[reeeee[1]] += 1
+
+print (x)
+
+print (y)
