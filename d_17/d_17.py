b6700b67e9001ea019fc9c8c0793afb69b858230
rng <rng@stuy.edu>
2018-12-17 18:38:17 -0500

    sos

--- d_17/d_17.py
+++ d_17/d_17.py
@@ -9,7 +9,7 @@ file_lines = list(lines)
 def process(o_line):
     c_line = o_line.rstrip("\n")
     c_line.split(",")
-    return c_line
+    return c_line[0], c_line[1]
 
 for i in file_lines:
     print(process(i))
