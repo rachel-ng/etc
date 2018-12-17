52561cbd7f3db0253e6dc99511c5840c72adb849
rng <rng@stuy.edu>
2018-12-17 18:44:10 -0500

    sos

--- d_17/d_17.py
+++ d_17/d_17.py
@@ -7,8 +7,10 @@ print (list(lines))
 file_lines = list(lines)
 
 def process(o_line):
-    c_line = o_line.rstrip("\n").replace("x","").replace("y","").replace("=","").split(",").strip()
-    return c_line[0], c_line[1]
+    c_line = o_line.rstrip("\n").replace("x","").replace("y","").replace("=","").split(",")
+    one = c_line[0].strip()
+    two = c_line[0].strip()
+    return one, two
 
 for i in file_lines:
     print(process(i))
